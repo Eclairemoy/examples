ffdcc3af7c8744ece5cd8c93b2b71cea2dae5026
Liz Moy <lizmoy@Lizs-MacBook-Pro.local>
2023-03-07 17:49:35 +0000

    adding finished psp demo

--- encrypted-multi-psp/index.js
+++ encrypted-multi-psp/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv').config()
+const Evervault = require('@evervault/sdk');
 
 const app = express();
 const port = process.env.PORT || 8080;
@@ -8,11 +9,32 @@ app.use(express.static('public'));
 app.use(express.json());
 
 const PaymentProviders = {
-  Stripe: require('./paymentProviders/stripe.js')
+  Stripe: require('./paymentProviders/stripe.js'),
+  CheckoutCom: require('./paymentProviders/checkoutcom.js'),
 }
 
+const evervault = new Evervault(process.env.EVERVAULT_API_KEY);
+const mySecret = process.env.EVERVAULT_API_KEY;
+evervault.enableOutboundRelay();
+
 app.post('/api/checkout', async (req, res) => {
-  // code goes here
+  const { card } = req.body
+  console.log(card)
+
+  const paymentOptions = {
+    card: card,
+    amount: 100,
+    currency: 'eur',
+    paymentReference: 'plant_purchase'
+  }
+
+  let paymentProvider = PaymentProviders.Stripe;
+  if (card.bin === "424242") {
+    paymentProvider = PaymentProviders.CheckoutCom;
+  }
+  const payment = await paymentProvider.makePayment(paymentOptions);
+
+  res.send(payment);
 });
 
 app.listen(port, () => {
